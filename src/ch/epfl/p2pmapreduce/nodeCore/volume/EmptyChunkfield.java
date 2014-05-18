e12475a0efddadd417689231bfee3a8dc9e47881
Valerian Pittet <valerian.pittet@epfl.ch>
2014-05-18 18:11:48 +0200

    comment removed

--- src/ch/epfl/p2pmapreduce/nodeCore/volume/EmptyChunkfield.java
+++ src/ch/epfl/p2pmapreduce/nodeCore/volume/EmptyChunkfield.java
@@ -28,12 +28,7 @@ public class EmptyChunkfield extends Chunkfield {
 	public void fillWithChunks(int[] globalChunkfield) {
 		throw new RuntimeException("fillWithChunks in empty Chunkfield");
 	}
-
-	/**
-	 * the user must be careful to provide chunkId in range of file's chunk count.
-	 * @param chunkId the chunk to look for.
-	 * @return true if this chunkfield contains the chunkId, false otherwise.
-	 */
+	
 	public boolean hasChunk(int chunkId) {
 		return false;
 	}
